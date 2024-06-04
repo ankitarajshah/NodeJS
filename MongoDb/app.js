6c4d7cba03ff5e558ffc217ba4da3e6e45695b9e
AnkiRajShah <k_anku@yahoo.co.in>
2024-06-04 11:56:06 +0530

    mongodb

--- MongoDb/app.js
+++ MongoDb/app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 const mongoConnect = require("./util/database").mongoConnect;
+const User = require("./models/user");
 
 const app = express();
 
@@ -18,19 +19,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  // User.findByPk(1)
-  //     .then((user) => {
-  //         req.user = user;
-  //         next();
-  //     })
-  //     .catch((err) => console.log(err));
-  next();
+  User.findById("635a9e6a9bc7ce61c0b80a1d")
+    .then((user) => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch((err) => console.log(err));
 });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-// app.use(errorController.get404);
+app.use(errorController.get404);
 
 mongoConnect(() => {
   app.listen(3000);
